c0d91edd1f1ceb9589f1f131d594f811e64cb126
emmanuelne <39935404+emmanuelnunoe@users.noreply.github.com>
2020-01-11 14:20:58 -0800

    se configuro completamente el plugin de jQuery, el estilo del mismo

---
+++ SIDI/resources/js/components/TableComponent.vue
@@ -0,0 +1,54 @@
+<template>
+<table class="table table-striped" id="myTable">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Nombre</th>
+      <th scope="col">Email</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr v-for="user in users">
+      <th scope="row">{{ user.id}}</th>
+      <td> {{ user.name }} </td>
+      <td> {{ user.email }} </td>
+    </tr>
+  </tbody>
+
+  
+</table>
+</template>
+
+<script>
+    import datatables from 'datatables'
+    export default {
+        mounted() {
+            this.getUsers();
+        },
+
+        data()
+        {
+            return{
+                  users: []
+
+            }
+        },
+
+        methods:{
+
+            myTable(){
+              $( function () {
+                $('#myTable').DataTable();
+                } );
+            },
+
+            getUsers(){
+                var urlUsers = "users";
+                axios.get(urlUsers).then(response=>{
+                    this.users = response.data;
+                    this.myTable()
+                 });
+            },
+        }
+    }
+</script>
