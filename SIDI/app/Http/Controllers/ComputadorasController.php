01ffc18adbbc1fa8d3a6b17719c9876a754e4b81
emmanuelne <39935404+emmanuelnunoe@users.noreply.github.com>
2020-01-15 17:49:59 -0800

    Se crearon los archivos necesarios para manejar el Modelo Computadoras; Controller, Model, Migrations

---
+++ SIDI/app/Http/Controllers/ComputadorasController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Computadoras;
+use Illuminate\Http\Request;
+
+class ComputadorasController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Computadoras  $computadoras
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Computadoras $computadoras)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Computadoras  $computadoras
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Computadoras $computadoras)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Computadoras  $computadoras
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Computadoras $computadoras)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Computadoras  $computadoras
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Computadoras $computadoras)
+    {
+        //
+    }
+}
