26fd85fadd8c07800a075b0ab22da1532275f838
emmanuelne <39935404+emmanuelnunoe@users.noreply.github.com>
2020-03-30 19:36:07 -0700

    Mejora en login despues de actualización, restauracion de paginas y estilo

--- SIDI/resources/views/computadoras/index.blade.php
+++ SIDI/resources/views/computadoras/index.blade.php
@@ -21,30 +21,6 @@
 <a href="{{ url('computadoras/create')}}" class="btn btn-success">Agregar computadora</a>
 </br>
 </br>
-<div class="cartas">
-    <div class="card text-white bg-danger mb-3" style="max-width: 18rem;">
-        <div class="card-header">Header</div>
-        <div class="card-body">
-            <h5 class="card-title">Danger card title</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        </div>
-    </div>
-    <div class="card text-white bg-warning mb-3" style="max-width: 18rem;">
-        <div class="card-header">Header</div>
-        <div class="card-body">
-            <h5 class="card-title">Warning card title</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        </div>
-    </div>
-    <div class="card text-white bg-info mb-3" style="max-width: 18rem;">
-        <div class="card-header">Header</div>
-        <div class="card-body">
-            <h5 class="card-title">Info card title</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        </div>
-    </div>
-</div>
-
 <!-- ===========================================================
         Tabla para desplegar el listado de computadoras
         registradas en el inventario
