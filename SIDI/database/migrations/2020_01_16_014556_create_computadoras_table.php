2a36bf8e80db8815856e7a309ecd727757e66fbf
Emmanuel <emmanuel.nuno.estrella@uabc.edu.mx>
2020-04-22 11:12:19 -0700

    update computadoras scheema
    Limpiando schema computadoras

--- SIDI/database/migrations/2020_01_16_014556_create_computadoras_table.php
+++ SIDI/database/migrations/2020_01_16_014556_create_computadoras_table.php
@@ -13,20 +13,8 @@ class CreateComputadorasTable extends Migration
      */
     public function up()
     {
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-   
-=======
->>>>>>> parent of 15c96e0... Migrations estan funcionando
-=======
->>>>>>> parent of 15c96e0... Migrations estan funcionando
-=======
->>>>>>> parent of 15c96e0... Migrations estan funcionando
-=======
-        Schema::enableForeignKeyConstraints();
->>>>>>> parent of 016532b... actualizacion migrations: foreign keys
+
+
         Schema::create('computadoras', function (Blueprint $table) {
             /*--------- DATOS DEL EQUIPO ------------- */
             $table->bigIncrements('id');
@@ -38,29 +26,9 @@ class CreateComputadorasTable extends Migration
             $table->string('procesadores');
             $table->string('velocidad');
             $table->integer('disco_duro');              //  capacidad en GB
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-            $table->integer('ram');                    //  capacidad en GB
-<<<<<<< HEAD
-=======
             $table->integer('ram');                     //  capacidad en GB
             $table->foreign('sistema_operativo')->references('id')->on('sistemas_operativos');
             $table->foreign('info_complementaria')->references('id')->on('informacion_complementaria_computadora');
->>>>>>> parent of 15c96e0... Migrations estan funcionando
-=======
-            $table->integer('ram');                     //  capacidad en GB
-            $table->foreign('sistema_operativo')->references('id')->on('sistemas_operativos');
-            $table->foreign('info_complementaria')->references('id')->on('informacion_complementaria_computadora');
->>>>>>> parent of 15c96e0... Migrations estan funcionando
-=======
-            $table->integer('ram');                     //  capacidad en GB
-            $table->foreign('sistema_operativo')->references('id')->on('sistemas_operativos');
-            $table->foreign('info_complementaria')->references('id')->on('informacion_complementaria_computadora');
->>>>>>> parent of 15c96e0... Migrations estan funcionando
-=======
-           // $table->foreignId('sistema_operativo')->references('id')->on('sistemas_operativo');
->>>>>>> parent of 016532b... actualizacion migrations: foreign keys
             $table->timestamps();
 
 
