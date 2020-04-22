c096a17848c22fee1579289749fadd982da2183f
Emmanuel <emmanuel.nuno.estrella@uabc.edu.mx>
2020-04-22 11:19:29 -0700

    Update 2020_04_07_033655_informacion_de_dominio.php
    trabajo de cambios  por rollback

--- SIDI/database/migrations/2020_04_07_033655_informacion_de_dominio.php
+++ SIDI/database/migrations/2020_04_07_033655_informacion_de_dominio.php
@@ -13,27 +13,12 @@ class InformacionDeDominio extends Migration
      */
     public function up()
     {
-<<<<<<< HEAD
-<<<<<<< HEAD
 
-
-        Schema::create('informacion_de_dominio', function(Blueprint $table) {
-            /*--------- INFORMACION DE DOMINIO------------- */
-            $table->unsignedInteger('id');
-            $table->unsignedInteger('computadora_id');
-            $table->foreign('computadora_id')->references('id')->on('computadoras')->onDelete('cascade');
-=======
-        Schema::create('informacion_de_dominio', function(Blueprint $table) {
-            /*--------- INFORMACION DE DOMINIO------------- */
-            $table->foreignId('computadoras');
->>>>>>> parent of 15c96e0... Migrations estan funcionando
-=======
-        Schema::dropIfExists('informacion_de_dominio');
+     
         Schema::create('informacion_de_dominio', function(Blueprint $table) {
             /*--------- INFORMACION DE DOMINIO------------- */
             $table->unsignedBigInteger('computadora_id');
             $table->foreign('computadora_id')->references('id')->on('computadoras');
->>>>>>> parent of 016532b... actualizacion migrations: foreign keys
             $table->boolean('unido_a_dominio');          // 0:si, 1:no
             $table->string('cuenta_dominio');
             $table->macAddress('mac_address');
@@ -53,5 +38,6 @@ class InformacionDeDominio extends Migration
     public function down()
     {
         //
+        Schema::dropIfExists('informacion_de_dominio');
     }
 }
