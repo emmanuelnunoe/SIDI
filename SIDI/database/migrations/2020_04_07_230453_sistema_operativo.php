87b9be4124c5797005c3bf63dbfea1c1984c59b4
Emmanuel <emmanuel.nuno.estrella@uabc.edu.mx>
2020-04-22 11:21:09 -0700

    Update 2020_04_07_230453_sistema_operativo.php
    trabajo de cabios por rollback

--- SIDI/database/migrations/2020_04_07_230453_sistema_operativo.php
+++ SIDI/database/migrations/2020_04_07_230453_sistema_operativo.php
@@ -15,35 +15,9 @@ class SistemaOperativo extends Migration
     {
         //
         Schema::create('sistemas_operativos', function(Blueprint $table) {
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-        $table->unsignedBigInteger('id');
-        $table->string('sitema_operativo');        //  descripcion
-=======
-        $table->bigIncrements('id');
-        $table->integer('sitema_operativo');        //  descripcion
->>>>>>> parent of 016532b... actualizacion migrations: foreign keys
-        $table->integer('edicion');                 //  tipo edicion
-=======
-        $table->id();
-        $table->integer('sitema_operativo');        //  catalogo de sistemas operativos
-        $table->integer('edicion');                 //  catalogo de edicion
->>>>>>> parent of 15c96e0... Migrations estan funcionando
-=======
-        $table->id();
-        $table->integer('sitema_operativo');        //  catalogo de sistemas operativos
-        $table->integer('edicion');                 //  catalogo de edicion
->>>>>>> parent of 15c96e0... Migrations estan funcionando
-=======
-        $table->id();
-        $table->integer('sitema_operativo');        //  catalogo de sistemas operativos
-        $table->integer('edicion');                 //  catalogo de edicion
->>>>>>> parent of 15c96e0... Migrations estan funcionando
-        $table->boolean('tipo_sistema');            //  0:32bit, 1:64bits
-        $table->integer('servicepack');
-        $table->boolean('licencia_activa');       //  0:si, 1:no
+
+        $table->increments('id');
+
         });
     }
 
